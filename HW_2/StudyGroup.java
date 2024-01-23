9c3e8c9f1155359f718469d6f5f88bca08db2643
dmitriytimoshenko73 <dmtimoshenko@mail.ru>
2024-01-23 17:00:00 +0300

    changed main

--- HW_2/StudyGroup.java
+++ HW_2/StudyGroup.java
@@ -1,10 +1,25 @@
 package HW_2;
 
-public class StudyGroup {
+// public class StudyGroup {
 
-    public StudyGroup(String string) {
+//     public StudyGroup(String string) {
+//     }
+
+//     public char[] name;
+
+// }
+public class StudyGroup {
+    String name;
+
+    public StudyGroup(String name) {
+        this.name = name;
     }
 
-    public char[] name;
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
 }
