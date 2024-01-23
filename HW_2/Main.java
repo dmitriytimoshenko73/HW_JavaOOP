7a1d9e466fb55a758d7dda2b2dd13edb6f2ccde7
dmitriytimoshenko73 <dmtimoshenko@mail.ru>
2024-01-23 16:45:32 +0300

    added main

--- HW_2/Main.java
+++ HW_2/Main.java
@@ -6,3 +6,31 @@
 // Модифицировать класс Контроллер, добавив в него метод сортирующий список потоков, путем вызова 
 // созданного сервиса. 
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        Stream stream1 = new Stream();
+        stream1.addStudyGroup(new StudyGroup("Группа 1"));
+        stream1.addStudyGroup(new StudyGroup("Группа 2"));
+
+        Stream stream2 = new Stream();
+        stream2.addStudyGroup(new StudyGroup("Группа 3"));
+
+        List<Stream> streams = new ArrayList<>();
+        streams.add(stream1);
+        streams.add(stream2);
+
+        Controler controler = new Controler();
+        controler.sortedStreams(streams);
+
+        for (Stream stream : streams) {
+            System.out.println("Количество групп: " + stream.studyGroups.size());
+            for (StudyGroup group : stream) {
+                System.out.println(group.name);
+            }
+            System.out.println();
+        }
+    }
+}
--- HW_2/StudyGroup.java
+++ HW_2/StudyGroup.java
@@ -2,4 +2,9 @@ package HW_2;
 
 public class StudyGroup {
 
+    public StudyGroup(String string) {
+    }
+
+    public char[] name;
+
 }
