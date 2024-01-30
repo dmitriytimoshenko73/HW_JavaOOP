e8298ee63ce07d3b4fdd3346f1eb0c366550595b
dmitriytimoshenko73 <dmtimoshenko@mail.ru>
2024-01-30 19:27:01 +0300

    added id

---
+++ HW_4/data/Id.java
@@ -0,0 +1,19 @@
+package HW_4.data;
+
+public class Id implements INumerate {
+    Long id;
+
+    public Id(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public Long get() {
+        return id;
+    }
+
+    @Override
+    public void set(Long id) {
+        this.id = id;
+    }
+}
