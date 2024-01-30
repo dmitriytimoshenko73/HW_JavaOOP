6f9c3140d44f78b0730d251366d25d617ce2be07
dmitriytimoshenko73 <dmtimoshenko@mail.ru>
2024-01-30 20:44:48 +0300

    added group

---
+++ HW_5/model/Group.java
@@ -0,0 +1,51 @@
+package HW_5.model;
+
+package model;
+
+import java.util.List;
+
+public class Group {
+
+    private int numberGroup;
+    private User teacherGroup;
+    private List<User> students;
+
+    public Group(int numberGroup, User teacherGroup, List<User> students) {
+        this.teacherGroup = teacherGroup;
+        this.students = students;
+        this.numberGroup = numberGroup;
+    }
+
+    public User getTeacherGroup() {
+        return teacherGroup;
+    }
+
+    public void setTeacherGroup(Teacher teacherGroup) {
+        this.teacherGroup = teacherGroup;
+    }
+
+    public List<User> getStudents() {
+        return students;
+    }
+
+    public void setStudents(List<User> students) {
+        this.students = students;
+    }
+
+    public int getNumberGroup() {
+        return numberGroup;
+    }
+
+    public void setNumberGroup(int numberGroup) {
+        this.numberGroup = numberGroup;
+    }
+
+    @Override
+    public String toString() {
+        return "Группа № "+ numberGroup +
+                 teacherGroup +
+                ", students:" + students +
+                '}';
+    }
+
+}
