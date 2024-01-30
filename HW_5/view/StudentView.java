eeb18e8490cea54a2bd194da26cceaaab919409b
dmitriytimoshenko73 <dmtimoshenko@mail.ru>
2024-01-30 20:54:54 +0300

    added studentview

---
+++ HW_5/view/StudentView.java
@@ -0,0 +1,10 @@
+package HW_5.view;
+
+import model.Student;
+
+public class StudentView {
+    public void printOnConsole ( Student student){
+        System.out.println(student.toString());
+    }
+    
+}
