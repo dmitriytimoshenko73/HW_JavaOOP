d9035515dbfaa856b3af755f7b43e668893a7fe0
dmitriytimoshenko73 <dmtimoshenko@mail.ru>
2024-01-30 18:46:07 +0300

    added main

---
+++ HW_4/Main.java
@@ -0,0 +1,44 @@
+// Создать класс УчительСервис и реализовать аналогично проделанному на семинаре.
+// Создать класс УчительВью и реализовать аналогично проделанному на семинаре.
+// Создать класс УчительКонтроллер и реализовать возможность создания, редактирования конкретного учителя и 
+// отображения списка учителей, имеющихся в системе. 
+
+package HW_4;
+
+import java.util.ArrayList;
+
+import java.util.Collections;
+
+import java.util.Iterator;
+
+import java.util.List;
+
+public static void main (String[] args) {
+
+    TeacherService teacherService = new TeacherService();{
+    // Добавление учителей в список 
+    teacherService.add(new Teacher("Иван Иванов", 10)); 
+    teacherService.add(new Teacher("Петр Петров", 5)); 
+    teacherService.add(new Teacher("Александр Сидоров", 20)); 
+    
+    // Вывод списка учителей по опыту работы 
+    teacherService.printTeachersByExperience();}
+}
+
+/*import java.util.ArrayList;
+
+import java.util.Collections;
+
+import java.util.Iterator;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        iUserService<Teacher> userService = new TeacherService();
+        TeacherController<Teacher> teacherController = new TeacherController<>(userService);
+        // Далее можно использовать методы из iUserController
+    }
+}
+    
+
