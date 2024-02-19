0e2bb63088462a40b416e3a39acd131a5e893968
dmitriytimoshenko73 <dmtimoshenko@mail.ru>
2024-02-19 17:49:09 +0300

    created main

---
+++ Final_HW/Main.java
@@ -0,0 +1,15 @@
+package Final_HW;
+
+public class Main {
+    
+
+    public static void main(String[] args) {
+    
+
+        System.out.println(new Controller().run());
+
+
+        
+    }
+    
+}
