d8d36f5e4badf102fad6decd32ea24dd7db89034
dmitriytimoshenko73 <dmtimoshenko@mail.ru>
2024-02-19 17:50:56 +0300

    created operationreader

---
+++ Final_HW/OperationReader.java
@@ -0,0 +1,23 @@
+package Final_HW;
+
+import java.util.Scanner;
+
+public class OperationReader {
+
+         
+    public OperationReader(String operation) {
+        getOperation();
+    }
+
+    public static String getOperation() {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите операцию: +, -, *, / ");
+        String oper = sc.nextLine();
+        sc.close();
+        return oper;
+
+    }
+    
+
+}
